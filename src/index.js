42766c6fcfc3e47e137c2d1219e40f21561e42df
Fiona Phillips <fiona.phillips@bel-mc-190916a.ad.unosquare.com>
2019-10-24 11:37:21 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -1,54 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Formik, Field, Form } from "formik";
 
-const initialValues = {
-  card: [
-    {
-      cardNum: "",
-      cardName: "",
-      expirDate: {
-        month: "",
-        year: ""
-      },
-      cvv: ""
-    }
-  ]
-};
+import CreditCardForm from './components/credit-card-form';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={values => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-            }, 500);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <div className="row">
-                <div className="col">
-                  <Field name="cardNum" type="text" />
-                </div>
-                <div className="col">
-                  <Field name="cardName" type="text" />
-                </div>
-                <div className="col">
-                  <Field name="expirDate.month" type="text" />
-                  <Field name="expirDate.year" type="text" />
-                  <Field name="cvv" type="text" />
-                </div>
-              </div>
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </Form>
-          )}
-        </Formik>
+          <CreditCardForm />
       </div>
     );
   }
